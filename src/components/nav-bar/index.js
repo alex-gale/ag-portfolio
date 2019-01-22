10f999028b7556995684c7228efce54b2c768459
Alex Gale <alexgaleiii@gmail.com>
2019-01-22 19:22:50 +0000

    Add logo to header

--- src/components/nav-bar/index.js
+++ src/components/nav-bar/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 import './index.scss'
+import Logo from '../../images/logo.svg'
 
 const NavBar = () => (
-  <div className="nav-bar transparent">
-    
+  <div className="nav-bar transparent" data-aos="fade-down" data-aos-duration="1000">
+    <img className="logo" src={Logo} alt="Alex Gale" />
   </div>
 )
 
