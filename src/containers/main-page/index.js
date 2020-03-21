2c92f0277ad93c12896faaf0c9abe78fe05eefa3
Alex Gale <alexgaleiii@gmail.com>
2020-03-21 13:55:05 +0000

    Design improvements
    * Changed all the fonts
    * Added a layout to the lower section of the site

--- src/containers/main-page/index.js
+++ src/containers/main-page/index.js
@@ -15,11 +15,40 @@ const MainPage = () => (
         </div>
       </div>
 
-			<a href="https://github.com/alex-gale" className="github-link">
-				<img src={GithubLogo} />
+			<a href="https://github.com/alex-gale" target="_blank" rel="noopener noreferrer" className="github-link">
+				<img alt="GitHub" src={GithubLogo} />
 				Visit my GitHub
 			</a>
     </div>
+
+		<div className="content">
+			<div className="about-container">
+				<div className="title">
+					<p>About Me</p>
+				</div>
+
+				<div className="about-content">
+					<div className="about-text">
+						<p>I'm a Full Stack web developer from Cardiff. My passion is writing clean code to power clean sites.</p>
+						<a href="mailto:alex01gale@gmail.com">Let's work together.</a>
+					</div>
+					<div className="image-holder">
+						<img alt="Alex Gale" src="https://www.colonialkc.org/wp-content/uploads/2014/07/Cory-Headshot-Outside-Square.jpg" />
+					</div>
+				</div>
+			</div>
+
+			<div className="tagline-container">
+				<p>clean code</p>
+				<p>clean sites</p>
+			</div>
+
+			<div className="skills-container">
+				<div className="title">
+					<p>What I Do</p>
+				</div>
+			</div>
+		</div>
   </div>
 )
 
